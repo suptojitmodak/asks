b4ad5e16e1d05aeb4bcefbfacce80d7890be3651
Suptojit Modak <suptojitmodak@gmail.com>
2025-07-08 04:45:12 +0600

    Update list.js

--- list.js
+++ list.js
@@ -1,12 +1,13 @@
 const urlParams = new URLSearchParams(location.search);
 const key = urlParams.get("key");
 
-if (key === "MY_SECRET_KEY") {
+if (document.currentScript && document.currentScript.tagName === 'SCRIPT' && key === "MY_SECRET_KEY") {
   window.redirectMap = {
     abc: "https://example.com",
     xyz: "https://google.com"
   };
   // Optional: console.clear();
 } else {
-  document.write("<h1>404 Not Found</h1>"); // ব্রাউজারে ভিজিট করলে শুধু এইটা দেখাবে
+  // সরাসরি ব্রাউজারে খোলা হয়েছে (বা iframe, অন্য কিছু)
+  document.body.innerHTML = "<h1>Access Denied or Nothing to see here</h1>";
 }
